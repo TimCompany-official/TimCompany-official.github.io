62042a2a3b8050d69423280bb4eeb6590e4145de
TimCompany-official <60646638+TimCompany-official@users.noreply.github.com>
2020-03-31 20:39:45 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,17 +1,24 @@
-var e = 0;
-function pow2(){
-	if (document.getElementById('pass').value == 1234){
-		e = confirm("пароль правильний\nввійти на котельню?");
-	}else{
-		alert("неправильний пароль")
-	} 
-	if (e){
-	alert('вхід виконаний')
-	document.location.replace("http://192.168.1.177");
-
-}else{
-	alert('щось не так')
+function kotelON() {
+  var x = new XMLHttpRequest();
+  x.open("GET", "http://192.168.1.177/$1", true);
+  x.send(null);
+  location.reload();
 }
-
+function kotelOFF() {
+  var x1 = new XMLHttpRequest();
+  x1.open("GET", "http://192.168.1.177/$2", true);
+  x1.send(null);
+  location.reload();
 }
-
+function boilerON() {
+  var x2 = new XMLHttpRequest();
+  x2.open("GET", "http://192.168.1.177/$3", true);
+  x2.send(null);
+  location.reload();
+}
+function boilerOFF() {
+  var x3 = new XMLHttpRequest();
+  x3.open("GET", "http://192.168.1.177/$4", true);
+  x3.send(null);
+  location.reload();
+}
